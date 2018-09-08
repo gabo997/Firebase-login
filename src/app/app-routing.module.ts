b1152841068358d5c2d1f6060fb752f60091527c
gabo997 <oliveiragabr@outlook.com>
2018-09-08 13:52:32 -0400

    Rutas agregadas!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+//rutas
+import { HomePageComponent } from "./components/home-page/home-page.component";
+import { LoginPageComponent } from "./components/login-page/login-page.component";
+import { NotFoundPageComponent } from "./components/not-found-page/not-found-page.component";
+import { RegisterPageComponent } from "./components/register-page/register-page.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: RegisterPageComponent},
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '**', component: NotFoundPageComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
